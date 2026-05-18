671c106acbf1ae7542dd03fcac7b65881fea4f75
Mohamed Fathah <mohammedfathah@gmail.com>
2026-05-18 23:09:26 +0530

    fix: build errors AppShell and ESLint config

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -6,9 +6,9 @@ import Header from './Header'
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen overflow-hidden">
-      <Sidebar />
+      <Sidebar isOpen={false} />
       <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
-        <Header />
+        <Header onMenuClick={() => {}} />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
         </main>
