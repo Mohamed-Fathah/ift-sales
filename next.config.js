d41f96804e2cb3fe7cff57530482d806b97b48bb
Mohamed Fathah <mohammedfathah@gmail.com>
2026-05-18 14:11:42 +0530

    fix: expose service role key in next.config and add fallback

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  },
+  experimental: { serverActions: { allowedOrigins: ['localhost:3000'] } },
   images: { domains: ['iftchennai.in', 'supabase.co'] },
 }
 module.exports = nextConfig
